b0285d3ed30b91535b855fe8c0ef06af223c6747
himanshu <himanshurajoriya9510@gmail.com>
2026-03-03 12:45:13 +0530

    last

--- app/extensions.py
+++ app/extensions.py
@@ -1,6 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
-from flask import request
 
 database = SQLAlchemy()
 jwt = JWTManager()
--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 app = createApp()
 CORS(app,
      supports_credentials=True,
-     origins=lambda origin: origin and origin.endswith("vercel.app"))
+     origins=["https://shop-esjt472l9-himanshus-projects-9a3d15ed.vercel.app/"],)
 with app.app_context():
     database.create_all()
 
