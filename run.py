08d2fc9136e0cd3cac1757ce37853b9f4e8b8dff
himanshu <himanshurajoriya9510@gmail.com>
2026-03-03 12:20:05 +0530

    last

--- run.py
+++ run.py
@@ -2,7 +2,9 @@ from app import createApp, database
 from flask_cors import CORS
 
 app = createApp()
-CORS(app, supports_credentials=True, origins=["https://vercel.app"])
+CORS(app,
+     supports_credentials=True,
+     origins=lambda origin: origin and origin.endswith("vercel.app"))
 with app.app_context():
     database.create_all()
 
